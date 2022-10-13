45d80bcd6ecb2f5b3fbc43b62ed3b29fd975cc33
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-13 10:09:51 +0200

    Bank osztály elkezdése

---
+++ src/hu/petrik/bankiszolgaltatasokOOP/Bank.java
@@ -0,0 +1,53 @@
+package hu.petrik.bankiszolgaltatasokOOP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Szamla> szamlaLista;
+
+    public Bank() {
+        this.szamlaLista = new ArrayList<>();
+    }
+
+    public Szamla szamlanyitas(Tulajdonos tulajdonos, int hitelKeret) {
+        //TODO: sokminden van még
+        Szamla s;
+        if () {
+
+        }
+    }
+
+    public long getOsszegyenleg(Tulajdonos tulajdonos) {
+        long ossz = 0;
+        for (Szamla s : this.szamlaLista) {
+            if (s.getTulajdonos().equals(tulajdonos)) {
+                ossz = s.getAktualisEgyenleg();
+            }
+        }
+        return ossz;
+    }
+
+    public Szamla getLegnagyobbEgyenleguSzamla(Tulajdonos tulajdonos) {
+        Szamla max = this.szamlaLista.get(0);
+        for (Szamla s : this.szamlaLista) {
+            if (s.aktualisEgyenleg > max.getAktualisEgyenleg()) {
+                max = s;
+            }
+        }
+        return max;
+    }
+
+
+    public long getOsszhitelkeret() {
+        long ossz = 0;
+        for (Szamla s : this.szamlaLista) {
+            if (s.getClass().equals(MegtakaritasiSzamla.class)) {
+                ossz = ossz + s.getAktualisEgyenleg();
+            }
+
+        }
+        return ossz;
+    }
+
+}
