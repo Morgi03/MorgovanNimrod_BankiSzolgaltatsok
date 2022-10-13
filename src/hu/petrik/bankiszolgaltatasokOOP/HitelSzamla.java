7d2151be20a566f8e63dfb3af4da0717d2974ef7
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-13 08:58:10 +0200

    HitelSzamla osztály elkészítése

---
+++ src/hu/petrik/bankiszolgaltatasokOOP/HitelSzamla.java
@@ -0,0 +1,26 @@
+package hu.petrik.bankiszolgaltatasokOOP;
+
+public class HitelSzamla extends Szamla {
+
+    private int hitelKeret;
+
+    public HitelSzamla(Tulajdonos tulajdonos, int hitelKeret) {
+        super(tulajdonos);
+        this.hitelKeret = hitelKeret;
+    }
+
+    public int getHitelKeret() {
+        return hitelKeret;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        if (this.hitelKeret - osszeg >= 0) {
+            this.hitelKeret -= osszeg;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
