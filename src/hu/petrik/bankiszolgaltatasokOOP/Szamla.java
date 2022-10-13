bddf7b85b0691e3fae35915f2461fb7e1f0a699a
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-13 08:51:25 +0200

    Szamla absztrakt osztály elkészítése

---
+++ src/hu/petrik/bankiszolgaltatasokOOP/Szamla.java
@@ -0,0 +1,26 @@
+package hu.petrik.bankiszolgaltatasokOOP;
+
+public abstract class Szamla extends BankiSzolgaltatas {
+
+    protected int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg) {
+
+    }
+
+    public abstract boolean kivesz(int osszeg);
+
+  /*  public Kartya ujKartya(String kartyaszam) {
+    return Kartya k = new Kartya(, kartyaszam);
+    }
+   */
+}
+
